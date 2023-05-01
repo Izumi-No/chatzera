ad757e21f4139692c741cf78356d21148b9b366b
Izumi <kevynmiguel@gmail.com>
2023-05-01 20:48:33 -0300

    fix: createUserUseCase injection

--- src/application/useCases/User/createUserUseCase.ts
+++ src/application/useCases/User/createUserUseCase.ts
@@ -1,5 +1,4 @@
 import { User } from "@/domain/entities/user/user";
-import { User } from "@/domain/entities/user/user";
 import { UserRepository } from "@/domain/repositories/userRepository";
 import { DrizzleUserRepository } from "@/infrastructure/persistence/drizzleOrm/repositories/drizzleUserRepository";
 import { UseCase } from "@/shared/application/useCase";
@@ -16,27 +15,7 @@ export class createUserUseCase
   }
 
   async execute(
-  private readonly userRepository: UserRepository;
-  constructor() {
-    this.userRepository = container.resolve("UserRepository");
-  }
-
-  async execute(
     request: createUserUseCase.Request
-  ): Promise<createUserUseCase.Response> {
-    const userOrError = User.create(request);
-
-    if (userOrError.isLeft()) {
-      throw userOrError.error;
-    }
-
-    await this.userRepository.save(userOrError.value);
-    return {
-      id: userOrError.value.id,
-      nickname: userOrError.value.nickname,
-      password: await userOrError.value.password.getHashedValue(),
-    };
-  }
   ): Promise<createUserUseCase.Response> {
     const userOrError = User.create(request);
 
